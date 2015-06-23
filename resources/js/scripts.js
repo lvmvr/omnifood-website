dcdc0240ab5cd1e1afc9fd3e3b6746cfebb19223
Lamar Glenn <lvmvr.x@gmail.com>
2015-06-23 16:50:58 -0400

    Add animation effects.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	/* Sticky Navigation */ 
+
 	$('.js--features').waypoint(function(direction){
 		if (direction == "down") {
 			$('nav').addClass('sticky');
@@ -10,4 +12,65 @@ $(document).ready(function(){
 		offset: '60px;' 
 	});
 
+	/* Scroll to section */
+
+	$('.js--scroll-to-plans').click(function(){
+		$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1500);
+	});
+
+	$('.js--scroll-to-start').click(function(){
+		$('html, body').animate({scrollTop: $('.js--features').offset().top}, 1000);
+	});
+
+	/* Smooth Scrolling to elements */
+
+	$(function() {
+  		$('a[href*=#]:not([href=#])').click(function() {
+    		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      		var target = $(this.hash);
+      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      		if (target.length) {
+	        		$('html,body').animate({
+	         		 scrollTop: target.offset().top
+	        		}, 1000);
+	        		return false;
+	      		}
+   			}
+ 		});
+	});
+
+	/* Animations on scroll */
+
+	$('.js--wp-1').waypoint(function(direction) {
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%'
+	});
+	$('.js--wp-2').waypoint(function(direction) {
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+	$('.js--wp-3').waypoint(function(direction) {
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '60%'
+	});
+	$('.js--wp-4').waypoint(function(direction) {
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%'
+	});
+
+
+
+
+
+
+
 });
+
+
+
+
+
