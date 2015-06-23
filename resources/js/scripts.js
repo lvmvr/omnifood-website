d2088498b41f6a0e18906c9c8853bd9f65403b53
Lamar Glenn <lvmvr.x@gmail.com>
2015-06-23 14:55:14 -0400

    Add waypoint for sticky nav.

---
+++ resources/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+	$('.js--features').waypoint(function(direction){
+		if (direction == "down") {
+			$('nav').addClass('sticky');
+		} else {
+			$('nav').removeClass('sticky');
+		}
+	}, {
+		offset: '60px;' 
+	});
+
+});
